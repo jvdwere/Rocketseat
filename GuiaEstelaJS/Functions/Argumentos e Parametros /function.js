6fe406a9a5683533bef359e35000f524ade39160
jvdwere <joao.victordwere@gmail.com>
2023-05-03 14:29:34 -0300

    beggin

---
+++ GuiaEstelaJS/Functions/Argumentos e Parametros /function.js
@@ -0,0 +1,11 @@
+// criar um aplicativo de frases motivacionais
+
+
+// declaracao da funcao 
+function frases() {
+    console.log("estudar é muito bom")
+    console.log("Pasciencia e percistencia")
+    console.log("Revisão é a mãe do aprendizado")
+}
+
+frases()
---
+++ GuiaEstelaJS/Functions/function.js
@@ -0,0 +1,12 @@
+// criar um aplicativo de frases motivacionais
+
+
+// declaracao da funcao 
+function frases() {
+    console.log("estudar é muito bom")
+    console.log("Pasciencia e percistencia")
+    console.log("Revisão é a mãe do aprendizado")
+    return "Revisão é a mãe do aprendizado"
+}
+
+frases()
